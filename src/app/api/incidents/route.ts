9738fcd93cb46da412ad207e2aeed25680416ad7
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-06-05 10:53:43 +0900

    feat: implement Datadog incidents API integration

---
+++ src/app/api/incidents/route.ts
@@ -0,0 +1,25 @@
+import { v2, client } from "@datadog/datadog-api-client";
+
+const datadogClientConfiguration = client.createConfiguration({
+  authMethods: {
+    apiKeyAuth: process.env.DATADOG_API_KEY || "",
+    appKeyAuth: process.env.DATADOG_APP_KEY || "",
+  },
+});
+
+datadogClientConfiguration.setServerVariables({
+  site: process.env.DATADOG_API_SITE || "datadoghq.com",
+});
+const apiInstance = new v2.IncidentsApi(datadogClientConfiguration);
+
+export async function GET() {
+  datadogClientConfiguration.unstableOperations["v2.listIncidents"] = true;
+
+  const res = await apiInstance.listIncidents();
+  return new Response(JSON.stringify(res), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
